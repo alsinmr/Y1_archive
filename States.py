b78a88f006f32c5fc8fb0b7f88d108dd6d6d94f5
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2024-03-23 13:55:20 +0100

    Added initial data processing

--- States.py
+++ States.py
@@ -23,3 +23,43 @@ trajs=[os.path.join(md_dir,f'apo{k}.xtc') for k in range(1,4)]
 for k,traj in enumerate(trajs):
     uni=Universe(topo,traj)
     states[f'run{k+1}']=states[f'run{k+1}'][:len(uni.trajectory)]
+    
+
+class StateChunk():
+    def __init__(self,states):
+        self.states=states
+        
+    def __call__(self,length=6000):
+        states=list()
+        runs=list()
+        starts=list()
+        counts=list()
+        
+        for k,x in enumerate(self.states.values()):
+            pos=0
+            while pos<len(x):
+                if pos+length<=len(x) and np.all(x[pos]==x[pos:pos+length]):
+                    runs.append(k)
+                    starts.append(pos)
+                    states.append(x[pos])
+                    counts.append(np.sum([s==states[-1] for s in states[:-1]]).astype(int))
+                    pos+=length
+                else:
+                    q=np.diff(x[pos:])!=0
+                    if np.any(q):
+                        pos+=np.argmax(q)+1
+                    else:
+                        pos=len(x)
+        
+        out={'run':runs,'start':starts,'state':states,'count':counts}
+        
+        i=np.argsort(states)
+        out={key:np.array(value)[i] for key,value in out.items()}
+        i=out['state']!=0
+        out={key:value[i] for key,value in out.items()}
+        
+        return out
+    
+chunks=StateChunk(states)
+                    
+        
--- basic_ana.py
+++ basic_ana.py
@@ -15,7 +15,7 @@ md_dir='/Volumes/My Book/Y1/apo'
 topo=os.path.join(md_dir,'prot.pdb')
 trajs=[os.path.join(md_dir,f'apo{k}.xtc') for k in range(1,4)]
 
-proj=pyDR.Project('Projects/basic')
+proj=pyDR.Project('Projects/basic',create=True)
 
 
 #%% Just HN for each run
@@ -25,13 +25,49 @@ for traj in trajs:
     sel.select_bond(Nuc='N')
     pyDR.md2data(sel)
     
+
+
+
+#%% Chunks with just one state
+from States import chunks
+
+length=6600
+for run,start,state,count in zip(*chunks(length).values()):
+    sel=pyDR.MolSelect(topo,trajs[run],project=proj)
+    sel.traj.t0=start
+    sel.traj.tf=start+length
+    sel.select_bond(Nuc='N')
+    pyDR.md2data(sel)
+    proj[-1].source.additional_info=f'state{state}_{count}'
+    
+    
 proj.detect.r_no_opt(12)
 proj.fit().detect.r_auto(6)
 proj['no_opt'].fit().opt2dist(rhoz_cleanup=True)
 
+#%% iRED ana for chunks
+length=6600
+for run,start,state,count in zip(*chunks(length).values()):
+    sel=pyDR.MolSelect(topo,trajs[run],project=proj)
+    sel.traj.t0=start
+    sel.traj.tf=start+length
+    sel.select_bond(Nuc='N')
+    pyDR.md2iRED(sel).iRED2data()
+    proj[-1].source.additional_info=f'state{state}_{count}'
+    
+    
+proj['iREDmode'].detect.r_no_opt(12)
+proj['iREDmode'].fit().detect.r_auto(6)
+proj['iREDmode']['no_opt'].fit().opt2dist(rhoz_cleanup=True)
+proj['iREDmode']['opt_fit'].modes2bonds()
 
-#%% Chunks with just one state
-length=6600  #Length of each ana
-run=[1,1,2]  #Which sim?
-start=[5900,12500,1170] #Starting frame
-state=[2,2,2]
+#%% Perform averaging
+from pyDR.misc.Averaging import avgDataObjs
+
+for state in range(1,4):
+    avgDataObjs(proj[f'o6:MD:state{state}.'])
+    
+for state in range(1,4):
+    avgDataObjs(proj[f'o6:IREDBOND:state{state}.'])
+    
+proj.save()
