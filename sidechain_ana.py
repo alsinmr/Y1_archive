e1cffe39925bbb1b695f2108c82405ce74fc2521
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2024-03-24 16:38:57 +0100

    Added iRED ana

--- sidechain_ana.py
+++ sidechain_ana.py
@@ -15,14 +15,14 @@ md_dir='/Volumes/My Book/Y1/apo'
 topo=os.path.join(md_dir,'prot.pdb')
 trajs=[os.path.join(md_dir,f'apo{k}.xtc') for k in range(1,4)]
 
-proj=pyDR.Project('Projects/basic',create=True)
+proj=pyDR.Project('Projects/sidechain',create=True)
 
 
 #%% Just HN for each run
 for traj in trajs:
     sel=pyDR.MolSelect(topo,traj,project=proj)
     sel.traj.tf=15000
-    sel.select_bond(Nuc='N')
+    sel.select_bond(Nuc='sidechain')
     pyDR.md2data(sel)
     
 
@@ -36,7 +36,7 @@ for run,start,state,count in zip(*chunks(length).values()):
     sel=pyDR.MolSelect(topo,trajs[run],project=proj)
     sel.traj.t0=start
     sel.traj.tf=start+length
-    sel.select_bond(Nuc='N')
+    sel.select_bond(Nuc='sidechain')
     pyDR.md2data(sel)
     proj[-1].source.additional_info=f'state{state}_{count}'
     
@@ -46,12 +46,11 @@ proj.fit().detect.r_auto(6)
 proj['no_opt'].fit().opt2dist(rhoz_cleanup=True)
 
 #%% iRED ana for chunks
-length=6600
 for run,start,state,count in zip(*chunks(length).values()):
     sel=pyDR.MolSelect(topo,trajs[run],project=proj)
     sel.traj.t0=start
     sel.traj.tf=start+length
-    sel.select_bond(Nuc='N')
+    sel.select_bond(Nuc='sidechain')
     pyDR.md2iRED(sel).iRED2data()
     proj[-1].source.additional_info=f'state{state}_{count}'
     
